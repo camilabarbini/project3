fe5871a125d2c532d38e8807345e0fb3f1d00e7b
Camila Barbini <camila.barbini@gmail.com>
2020-05-06 12:45:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,3 @@
-const express = require('express');
-const server = express();
-const bodyParser = require('body-parser');
-
-require('./database/index');
-require('./authorization');
-
-server.use(bodyParser.json());
-
-server.listen(3000, () => {
-    console.log('servidor iniciado...');
-});
-
-server.use(bodyParser.json());
-
-
-
 
  
 
