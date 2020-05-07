ec1a6810076f00a4ee6a23ad367aebf663ecb120
Camila Barbini <camila.barbini@gmail.com>
2020-05-07 14:36:21 -0300

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,1 +1,29 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mysql://sNUbrPBJdI:GdTXgKAFV9@remotemysql.com:3306/sNUbrPBJdI');
 
+(async function databaseAuthenticate(){
+    try{
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+})()
+
+
+/* sequelize.authenticate().then(async()=>{
+    const query = 'SELECT * FROM CAMILA'
+    const results = await sequelize.query(query, { raw: true});
+    console.log(results)
+}); */
+
+
+
+//sequelize.query('SELECT * FROM CAMILA',{
+  //  type:sequelize.QueryTypes.SELECT}
+//).then(function(resultados){res.json(resultados)
+/* sequelize.authenticate().then(async()=>{
+const query = 'SELECT * FROM CAMILA'
+await sequelize.query(query, { raw: true}); */
+//res.json(results)
+//})
